7de64419a5c0d47af7db66d3c5be1ec9790f5f1c
Sonagnon Obed AGBOHOUN <obedagbohouns@gmail.com>
2025-10-23 18:12:56 +0100

    blog and config tailwindcss

---
+++ js/tailwind-config.js
@@ -0,0 +1,15 @@
+// Configuration Tailwind CSS centralisée
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#33CC00",
+        "gray-custom": "#575757",
+      },
+      fontFamily: {
+        exo: ["Exo", "sans-serif"],
+        "open-sans": ["Open Sans", "sans-serif"],
+      },
+    },
+  },
+};
